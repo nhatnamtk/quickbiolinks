fac831ff250dd5106c3717d150f8ea900215ad16
McJoe21 <39857764+McJoe21@users.noreply.github.com>
2022-03-15 13:44:15 +0000

    Create config.js

---
+++ templates/simple/js/config.js
@@ -0,0 +1,16 @@
+var config = {
+  tag: "@yourname",
+  name: "Your Name",
+  email: "you@mail.com",
+  pic: "images/profile-picture.webp",
+  links:[
+    {
+      title: "GitHub",
+      link: "https://github.com/mcjoe21"
+    },
+        {
+      title: "Link 2",
+      link: "https://other-link.site"
+    }
+    ]
+}
